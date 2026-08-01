8bdbb7358c47cb36f0cea2458d04734c2c954de1
Dave Stewart <dave@davestewart.co.uk>
2026-08-01 21:54:42 +0100

    feat: shared demo package (styles, navigation, helpers); dedupe demo css

---
+++ demos/shared/index.ts
@@ -0,0 +1,8 @@
+export { applyDrag, generateItems, type DragResult } from './helpers'
+export {
+  navigation,
+  pages,
+  defaultPage,
+  type DemoPage,
+  type DemoSection,
+} from './navigation'
